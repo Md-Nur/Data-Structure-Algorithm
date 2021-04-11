5a1849e2db3fa86d73d5cd98a1e6034ae4934ed8
Muhammad Nur <md.nurealamsiddiquee2004@gmail.com>
2021-04-11 22:14:44 +0600

    hobe

--- 52 Problem solving/16_shobdoBiporjoy.cpp
+++ 52 Problem solving/16_shobdoBiporjoy.cpp
@@ -4,51 +4,26 @@
 
 using namespace std;
 
-int wordCount(char word[100]);
-
 int main()
 {
-    int T, a, b, word;
-    char sentence[1000], temp;
-
-    cin >> T;
-    while (T--)
+    int t;
+    char sentence[1002], word[1002];
+    cin >> t;
+    while (t--)
     {
         getchar();
-        cin.get(sentence, 1000);
-        cout << sentence << endl;
-
-        a = 0;
-        word = wordCount(sentence);
-        for (int i = 0; i < word; i++)
+        cin.get(sentence, 1002);
+        // cout << sentence << endl;
+        for (int i = 0; i < strlen(sentence); i++)
         {
-            b = a;
-            while (sentence[a] != ' ')
+            if (sentence[i]!=' ')
             {
-                a++;
+                word[i]= sentence[i];
             }
-            for (int j = b; sentence[j] != ' '; j++)
-            {
-                temp = sentence[j];
-                sentence[j] = sentence[a - j];
-                sentence[a - j] = temp;
-            }
+            
         }
-        cout << sentence << endl;
+        
     }
 
     return 0;
-}
-
-int wordCount(char word[100])
-{
-    int count = 1;
-    for (int i = 0; i < strlen(word); i++)
-    {
-        if (word[i] == ' ')
-        {
-            count++;
-        }
-    }
-    return count;
 }
