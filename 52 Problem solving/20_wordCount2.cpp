8778753e76122f0349b9108e7e2b214d9c01ff04
Muhammad Nur <md.nurealamsiddiquee2004@gmail.com>
2021-04-26 10:12:59 +0600

    hobe

---
+++ 52 Problem solving/20_wordCount2.cpp
@@ -0,0 +1,52 @@
+//Program Name: Word count(only letters and spaces)
+
+/*	====	included header file	====	*/
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <cmath>
+#include <string>
+
+/*	====	Predefine Marcos & namespaces	====	*/
+using namespace std;
+
+/*	====	Stucture & Uninons	====	*/
+
+/*	====	Declaration of classes	====	*/
+
+/*	====	Declaration of Functions	====	*/
+
+/*	====	Classes	====	*/
+
+/*	====	Functions	====	*/
+
+/*	+-+-+-+-+-+-+-+-+-+-+
+        Main Function
+    +-+-+-+-+-+-+-+-+-+-+	*/
+int main()
+{
+    int t;
+    cin >> t;
+    getchar();
+    while (t--)
+    {
+        string line;
+        int count = 1, len;
+        getline(cin, line);
+        // cout << line << endl;
+        len = line.length();
+        for (int i = 0; i < len; i++)
+        {
+            if (line.at(i) == ' ')
+            {
+                count++;
+                while (line.at(i) == ' ' || line.at(i) == ',' || line.at(i) == ';' || line.at(i) == '\'' || line.at(i) == '!')
+                {
+                    i++;
+                }
+            }
+        }
+        cout << "Count = " << count << endl;
+    }
+    return 0;
+}
