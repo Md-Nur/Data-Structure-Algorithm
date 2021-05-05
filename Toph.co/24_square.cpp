34a4648044ea503284a96304d5d9c166e2136f33
Muhammad Nur <md.nurealamsiddiquee2004@gmail.com>
2021-05-05 18:13:25 +0600

    oe

---
+++ Toph.co/24_square.cpp
@@ -0,0 +1,33 @@
+//Program Name: Find square
+
+/*	====	included header file	====	*/
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include <fstream>
+#include <vector>
+#include <list>
+#include <map>
+
+/*	====	Stucture, Uninons, Predefine Marcos & namespaces	====	*/
+using namespace std;
+
+/*	====	Declaration of classes & Functions	====	*/
+
+/*	====	Classes	====	*/
+
+/*	====	Functions	====	*/
+
+/*	+-+-+-+-+-+-+-+-+-+-+
+        Main Function
+    +-+-+-+-+-+-+-+-+-+-+	*/
+int main()
+{
+    int a;
+    cin>>a;
+    cout << a*a << endl;
+    return 0;
+}
