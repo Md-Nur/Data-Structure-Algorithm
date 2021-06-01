0544abdfa76ebffd3f23bb5045f0b99f4042e08c
Muhammad Nur <md.nurealamsiddiquee2004@gmail.com>
2021-06-01 12:08:33 +0600

    hobe

--- Hacker Rank/02_simpleArraySum.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-m = input().split()
-add = 0
-
-for i in range(n):
-    add = add + int(m[i])
-
-print(add)
---
+++ ICT.c
@@ -0,0 +1,14 @@
+//Programm name: Calculatons
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    printf("Enter 2 Numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("Addition = %d,\n", a + b);
+    printf("Subtraction = %d,\n", a - b);
+    printf("Multiplication = %d,\n", a * b);
+    printf("Division = %d,\n", a / b);
+    return 0;
+}
+
